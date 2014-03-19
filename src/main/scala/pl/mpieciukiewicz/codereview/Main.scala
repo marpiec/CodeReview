1f8dc663c8d64b831d83be7acaef6d0ffc730a3e
Marcin Pieciukiewicz (mp18617) <marcin.pieciukiewicz@citi.com>
2014-03-19 11:46:44 +0100

    Getting file content after commit

--- src/main/scala/pl/mpieciukiewicz/codereview/Main.scala
+++ src/main/scala/pl/mpieciukiewicz/codereview/Main.scala
@@ -1,10 +1,40 @@
 package pl.mpieciukiewicz.codereview
 
+import java.net.{InetSocketAddress, SocketAddress, URI, ProxySelector}
+import java.{util, net}
+import java.io.IOException
+
 /**
  * @author Marcin Pieciukiewicz
  */
-object Main extends App {
+object Main {
 
-  Playground.start()
+  def main(args: Array[String]) {
+
+    defineProxy()
+
+    Playground.start()
+  }
+
+
+
+
+  def defineProxy() {
+
+    System.setProperty("http.proxyHost", "webproxy.ssmb.com")
+    System.setProperty("http.proxyPort", "8080")
+
+    ProxySelector.setDefault(new ProxySelector() {
+      override def connectFailed(uri: URI, sa: SocketAddress, ioe: IOException) {
+        if (uri == null || sa == null || ioe == null) {
+          throw new IllegalArgumentException("Arguments can't be null.")
+        }
+      }
+      override def select(uri: URI): java.util.List[java.net.Proxy] = {
+        java.util.Arrays.asList(new java.net.Proxy(java.net.Proxy.Type.HTTP, InetSocketAddress.createUnresolved("webproxy.ssmb.com", 8080)))
+      }
+    })
+  }
+
 
 }
--- src/main/scala/pl/mpieciukiewicz/codereview/Playground.scala
+++ src/main/scala/pl/mpieciukiewicz/codereview/Playground.scala
@@ -8,6 +8,8 @@ import org.eclipse.jgit.treewalk.TreeWalk
 import org.eclipse.jgit.revwalk.RevCommit
 import org.eclipse.jgit.lib.Repository
 import org.eclipse.jgit.diff.{DiffFormatter, RawTextComparator}
+import org.apache.commons.io.FileUtils
+import org.eclipse.jgit.treewalk.filter.PathFilter
 
 /**
  *
@@ -18,7 +20,8 @@ object Playground {
 
     val repoDir = new File("c:/TmpRepo/")
 
-    repoDir.mkdir()
+
+  //  cloneRepo(repoDir)
 
     val builder = new FileRepositoryBuilder()
     implicit val repository = builder.findGitDir(repoDir).build()
@@ -30,7 +33,7 @@ object Playground {
     val filesInCommits = commits.map(getFilePathsFromCommit)
     println(filesInCommits)
 
-  val commit = commits.head
+    val commit = commits.head
 
     val out = new ByteArrayOutputStream()
     val df = new DiffFormatter(out)
@@ -46,6 +49,26 @@ object Playground {
     println(diffs)
     println(out.toString("UTF-8"))
 
+    val treeWalk = new TreeWalk(repository)
+    treeWalk.addTree(commit.getTree)
+    treeWalk.setRecursive(true)
+    treeWalk.setFilter(PathFilter.create("app/view/calculator.html"))
+
+    if (!treeWalk.next()) {
+      throw new IllegalStateException(s"Did not find expected file ${filesInCommits.head.head}")
+    }
+
+    val objectId = treeWalk.getObjectId(0)
+    val loader = repository.open(objectId)
+
+    val content = new String(loader.getBytes)
+
+    println(content)
+
+
+    repository.close()
+
+
   }
 
   def getFilePathsFromCommit(commit: RevCommit)(implicit repository: Repository): List[String] = {
@@ -60,6 +83,7 @@ object Playground {
   }
 
   def cloneRepo(directory: File) {
+    FileUtils.deleteDirectory(directory)
     Git.cloneRepository().
       setURI("https://github.com/marpiec/AngularJSCalculator.git").
       setDirectory(directory).
