10a5301b961cf6b40a14223023b6c8a2b56c6a54
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2014-03-22 14:43:15 +0100

    Migrated assertions to fest assertions

--- src/test/scala/pl/mpieciukiewicz/codereview/git/GitDiffParserSpec.scala
+++ src/test/scala/pl/mpieciukiewicz/codereview/git/GitDiffParserSpec.scala
@@ -3,6 +3,8 @@ package pl.mpieciukiewicz.codereview.git
 import org.scalatest.{GivenWhenThen, FeatureSpec}
 import scala.io.Source
 import pl.mpieciukiewicz.codereview.vcs.{LineAdded, LineDeleted}
+import org.fest.assertions.api.Assertions._
+import collection.JavaConverters._
 
 /**
  *
@@ -21,22 +23,20 @@ class GitDiffParserSpec extends FeatureSpec with GivenWhenThen {
       val parsed = parser.parse(diff)
 
       Then("Has correct content")
-     // assert(parsed.fromFileName == "app/js/app.js")
-     // assert(parsed.toFileName == "app/js/app.js")
 
-      assert(parsed.changedLines == List(LineDeleted(26, "    $scope.valueA = 0;                          //first value for given operation"),
-                                        LineDeleted(27, "    $scope.valueB = 0;                          //second value for given operation"),
-                                        LineAdded(26, "    $scope.valueA = 0;                          //first (left) value that will be used for computation"),
-                                        LineAdded(27, "    $scope.valueB = 0;                          //second (right) value that will be used for computation"),
-                                        LineDeleted(85, "        $scope.displayValue = Math.floor($scope.selectedOperation($scope.valueA, $scope.valueB));"),
-                                        LineDeleted(86, "        $scope.clearValue = true;"),
-                                        LineDeleted(87, "        $scope.valueA = $scope.displayValue;"),
-                                        LineAdded(85, "        if($scope.selectedOperation!=null) {"),
-                                        LineAdded(86, "            $scope.displayValue = Math.floor($scope.selectedOperation($scope.valueA, $scope.valueB));"),
-                                        LineAdded(87, "            $scope.clearValue = true;"),
-                                        LineAdded(88, "            $scope.valueA = $scope.displayValue;"),
-                                        LineAdded(89, "        }")
-      ))
+      assertThat(parsed.changedLines.asJava).containsExactly(
+        LineDeleted(26, "    $scope.valueA = 0;                          //first value for given operation"),
+        LineDeleted(27, "    $scope.valueB = 0;                          //second value for given operation"),
+        LineAdded(26, "    $scope.valueA = 0;                          //first (left) value that will be used for computation"),
+        LineAdded(27, "    $scope.valueB = 0;                          //second (right) value that will be used for computation"),
+        LineDeleted(85, "        $scope.displayValue = Math.floor($scope.selectedOperation($scope.valueA, $scope.valueB));"),
+        LineDeleted(86, "        $scope.clearValue = true;"),
+        LineDeleted(87, "        $scope.valueA = $scope.displayValue;"),
+        LineAdded(85, "        if($scope.selectedOperation!=null) {"),
+        LineAdded(86, "            $scope.displayValue = Math.floor($scope.selectedOperation($scope.valueA, $scope.valueB));"),
+        LineAdded(87, "            $scope.clearValue = true;"),
+        LineAdded(88, "            $scope.valueA = $scope.displayValue;"),
+        LineAdded(89, "        }"))
 
     }
     
