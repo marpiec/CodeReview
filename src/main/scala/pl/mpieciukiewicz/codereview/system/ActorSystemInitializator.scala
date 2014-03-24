20848dd9a291e526cdc8e3fb0482512b472de418
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2014-03-24 23:36:32 +0100

    Playing with actor system

--- src/main/scala/pl/mpieciukiewicz/codereview/system/ActorSystemInitializator.scala
+++ src/main/scala/pl/mpieciukiewicz/codereview/system/ActorSystemInitializator.scala
@@ -1,6 +1,9 @@
 package pl.mpieciukiewicz.codereview.system
 
 import akka.actor.{ActorSystem, Props, ActorContext}
+import pl.mpieciukiewicz.codereview.database.UserStorage
+import pl.mpieciukiewicz.codereview.database.engine.{DatabaseAccessor, DocumentDataStorage}
+import pl.mpieciukiewicz.codereview.utils.json.JsonUtil
 
 /**
  * @author Marcin Pieciukiewicz
@@ -9,7 +12,12 @@ class ActorSystemInitializator {
 
   def createActors(context: ActorSystem) {
 
-    context.actorOf(Props[UserManager], "userManager")
+
+    val documentDataStorage = new DocumentDataStorage(new DatabaseAccessor("jdbc:h2:mem:testdb", "sa", "sa"), new JsonUtil)
+
+    val userStorage = new UserStorage(documentDataStorage)
+
+    context.actorOf(Props(classOf[UserManager], userStorage), "userManager")
 
   }
 
