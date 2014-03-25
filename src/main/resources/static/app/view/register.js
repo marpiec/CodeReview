a6d2420c043b3d9551a378ea6dbf3c9db7132821
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2014-03-25 23:06:22 +0100

    Improved registration page

--- src/main/resources/static/app/view/register.js
+++ src/main/resources/static/app/view/register.js
@@ -5,12 +5,20 @@ app.controller("RegisterController", function ($scope, $http) {
     $scope.password = "";
     $scope.passwordRepeat = "";
 
+    $scope.registerFormVisible = true;
+    $scope.registrationSuccessfulVisible = false;
+    $scope.userAlreadyRegistered = false;
+
     $scope.registerUser = function() {
 
         if($scope.password == $scope.passwordRepeat) {
             $http.post("/rest/register-user", "", {params: {name: $scope.userName, email: $scope.email, password: $scope.password}}).
                 success(function (data, status, headers, config) {
-                    alert("OK "+data)
+                    if(data == "UserRegistered") {
+                        userRegistered();
+                    } else if (data == "UserAlreadyExists") {
+                        userAlreadyExists();
+                    }
                 }).
                 error(function (data, status, headers, config) {
                     alert("Error communication with server!")
@@ -20,6 +28,18 @@ app.controller("RegisterController", function ($scope, $http) {
             alert("Passwords don't match!")
         }
 
+    };
+
+    function userRegistered() {
+        $scope.registerFormVisible = false;
+        $scope.registrationSuccessfulVisible = true;
+        $scope.userAlreadyRegistered = false;
+    }
+
+    function userAlreadyExists() {
+        $scope.registerFormVisible = false;
+        $scope.registrationSuccessfulVisible = false;
+        $scope.userAlreadyRegistered = true;
     }
 
 
