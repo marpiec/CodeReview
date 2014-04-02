f042344cfa9054dd93048f41bbf7a70f19d1dcc9
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2014-04-02 21:12:10 +0200

    Configuration test created

---
+++ src/test/scala/pl/mpieciukiewicz/codereview/utils/ConfigurationSpec.scala
@@ -0,0 +1,33 @@
+package pl.mpieciukiewicz.codereview.utils
+
+import org.scalatest.{GivenWhenThen, FeatureSpec}
+import org.fest.assertions.api.Assertions._
+
+class ConfigurationSpec extends FeatureSpec with GivenWhenThen {
+
+  feature("Provides proper configuration") {
+
+    scenario("Loads proper default project configuration") {
+
+      Given("Configuration")
+      val config = Configuration.fromClasspath("/application.conf")
+
+      Then("Configuration contains proper options")
+
+      assertThat(config.webServer.port).isEqualTo(8080)
+
+      assertThat(config.proxy.enabled).isFalse
+      assertThat(config.proxy.host).isEqualTo("some.proxy.com")
+      assertThat(config.proxy.port).isEqualTo(8080)
+
+      assertThat(config.storage.dataDirectory).isEqualTo("../data/")
+
+      Then("String representation is not empty")
+
+      assertThat(config.toString).isNotEmpty
+
+    }
+
+  }
+
+}
