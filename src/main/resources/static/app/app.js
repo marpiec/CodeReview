6438fe288850fe668698c9d6e2052aa2447fada0
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2014-04-10 00:02:11 +0200

    Style added for login page

--- src/main/resources/static/app/app.js
+++ src/main/resources/static/app/app.js
@@ -61,3 +61,14 @@ app.run(function($rootScope, $location, session) {
     });
 });
 
+
+app.directive('autoFocus', function($timeout) {
+    return {
+        restrict: 'AC',
+        link: function(_scope, _element) {
+            $timeout(function(){
+                _element[0].focus();
+            }, 0);
+        }
+    };
+});
