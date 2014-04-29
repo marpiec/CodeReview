bf8241dc889ef12d5210dda163a8b6f6ed97b233
Marcin Pieciukiewicz (mp18617) <marcin.pieciukiewicz@citi.com>
2014-04-29 17:19:41 +0200

    Loading all commits

--- src/main/resources/static/app/view/repository.js
+++ src/main/resources/static/app/view/repository.js
@@ -1,21 +1,28 @@
 app.controller("RepositoryController", function ($scope, $http, $routeParams, secureService) {
 
+    var pageSize = 20;
     var repositoryId = parseInt($routeParams.repositoryId);
 
     $scope.repositoryId = repositoryId;
-
     $scope.commits = [];
+    $scope.loadMoreCommitsButtonVisible = true;
 
     function init() {
-        secureService.get("/rest/commits/"+repositoryId+"/0/20", false, handleLoadCommitsResponse);
+        $scope.loadMoreCommits();
     }
 
     function handleLoadCommitsResponse(response) {
-        $scope.commits = response.commits;
+        $scope.commits = $scope.commits.concat(response.commits);
+        if(response.commits.length < pageSize) {
+            $scope.loadMoreCommitsButtonVisible = false;
+        }
     }
+
+    $scope.loadMoreCommits = function() {
+        secureService.get("/rest/commits/"+repositoryId+"/"+$scope.commits.length+"/"+pageSize, false, handleLoadCommitsResponse);
+    };
 
 
     init();
-
 
 });
